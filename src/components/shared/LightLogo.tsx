b0e2465cf398c7f533cbdf44ad7a2a0d8a5c63cd
denimozh <denimozh@gmail.com>
2024-05-25 01:55:24 +0100

    Finished Setting UpAppwrite

--- src/components/shared/LightLogo.tsx
+++ src/components/shared/LightLogo.tsx
@@ -4,7 +4,7 @@ const LightLogo = () => {
   return (
     <div className='flex flex-row items-center'>
         <img src='/assets/iconLight.png' className='w-16 h-15'/>
-        <p className='text-4xl font-medium'>Poupee Passion</p>
+        <p className='text-4xl font-medium'>PoupeePassion</p>
     </div>
   )
 }
