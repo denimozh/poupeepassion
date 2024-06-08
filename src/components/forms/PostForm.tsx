6f09db25f994acfb19fb85d94d82616e127bde53
denimozh <denimozh@gmail.com>
2024-06-08 23:09:09 +0100

    Added form and started to make create Posts

---
+++ src/components/forms/PostForm.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from '../ui/textarea'
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+const PostForm = () => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className=''>Title</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Title" {...field} />
+                            </FormControl>
+                            <FormMessage className='text-red-500'/>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="caption"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className=''>Caption</FormLabel>
+                            <FormControl>
+                                <Textarea className='h-36 bg-slate-100 rounded-xl outline-none' {...field} />
+                            </FormControl>
+                            <FormMessage className='text-red-500'/>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default PostForm
--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -10,19 +10,19 @@ const Bottombar = () => {
   return (
     <section className='border-t-2 border-slate-400 z-50 flex flex-row w-full justify-between sticky bottom-0 px-3 py-1 md:hidden'>
         <Link to={"/"} className="active:bg-slate-100 gap-4 flex items-center py-4 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
-          <GoHome className='text-3xl'/>
+          <GoHome className='text-2xl'/>
         </Link>
-        <Link to={"/explore"} className="gap-4 flex items-center py-4 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
-          <HiOutlineSquares2X2 className='text-3xl'/>
+        <Link to={"/explore"} className="gap-4 flex items-center py-3 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
+          <HiOutlineSquares2X2 className='text-2xl'/>
         </Link>
-        <Link to={"/saved"} className="gap-4 flex items-center py-4 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
-          <BiBookmark className='text-3xl'/>
+        <Link to={"/saved"} className="gap-4 flex items-center py-3 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
+          <BiBookmark className='text-2xl'/>
         </Link>
-        <Link to={"/messages"} className="gap-4 flex items-center py-4 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
-          <IoPaperPlaneOutline className='text-3xl'/>
+        <Link to={"/messages"} className="gap-4 flex items-center py-3 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
+          <IoPaperPlaneOutline className='text-2xl'/>
         </Link>
-        <Link to={"/create-post"} className="gap-4 flex items-center py-4 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
-          <IoCreateOutline className='text-3xl'/>
+        <Link to={"/create-post"} className="gap-4 flex items-center py-3 px-3 hover:bg-slate-100 text-lg rounded-lg hover:font-semibold">
+          <IoCreateOutline className='text-2xl'/>
         </Link>
     </section>
   )
