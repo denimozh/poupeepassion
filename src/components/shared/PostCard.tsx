b489f8b41941150d25fc5fdf1d8759ba521b049b
denimozh <denimozh@gmail.com>
2024-06-16 23:55:08 +0100

    Post Card

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,3 +1,4 @@
+import { multiFormatDateString } from '@/lib/utils';
 import { Models } from 'appwrite'
 import React from 'react'
 import { Link } from 'react-router-dom';
@@ -12,9 +13,21 @@ const PostCard = ({ post }: PostCardProps) => {
       <div className='flex justify-between items-center'>
         <div className='flex items-center gap-3'>
           <Link to={`/profile/${post.creator.$id}`}>
-            <img />
+            <img src={post?.creator?.imageUrl} alt='creator' className='rounded-full w-12 lg:h-12'/>
           </Link>
+
+          <div className='flex flex-col'>
+            <p className='text-lg'>{post.creator.username}</p>
+            <div>
+              <p className='text-slate-400 text-[14px]'>
+                {multiFormatDateString(post.$createdAt)}
+              </p>
+            </div>
+          </div>
         </div>
+        <Link to={`/update/post/${post.$id}`}>
+          
+        </Link>
       </div>
     </div>
   )
